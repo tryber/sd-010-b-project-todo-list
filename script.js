47576a75010d7afbcf93e9b24de597820a93b664
lucas <peixotolucas516@gmail.com>
2021-03-28 09:55:58 -0300

    requisito 9 concluído

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-
-
-
-
-
-
-
 function addNewTask() { //cria lista de tarefas apertando botao
   const getInputField = document.querySelector('#texto-tarefa'); 
   const addInputButton = document.querySelector('#criar-tarefa');
   const getTaskList = document.querySelector('#lista-tarefas');
+  const eraseAllButton = document.querySelector('#apaga-tudo')
+
+  eraseAllButton.addEventListener('click', function(event) {
+    event.target.classList.removeChild('#lista-tarefas')
+    
+  })
 
   addInputButton.addEventListener('click', function() {
     if (getInputField.value.length > 0) {
@@ -17,18 +16,27 @@ function addNewTask() { //cria lista de tarefas apertando botao
 
       getTaskList.appendChild(newLi);
       getInputField.value = '';
-      newLi.addEventListener('click', function(event){
-        //pintar a li de cinza e despintar as outras se houver
+      newLi.addEventListener('click', function(event) {
+        // pintar a li de cinza e despintar as outras se houver
         let selected = document.querySelector('.selected')
         if (selected != null) {
           selected.classList.remove('selected')
         }
         event.target.classList.add('selected')
       })
-    } 
-    
+      newLi.addEventListener('dblclick', function(event){
+        let completed = document.querySelector('.completed')
+        event.target.classList.add('completed')
+        if (completed != null) {
+          newLi.addEventListener('dblclick' , function(event) {
+            event.target.classList.remove('completed')
+          })
+        }
+      })
+      
+    }    
+      
   })
-
   
 };
 
@@ -36,25 +44,4 @@ addNewTask();
 
 
 
-let listaBonita = document.querySelector('#lista-tarefas')
-function listMudaCor() {
-  for (let i = 0; i < listaBonita.length; i += 1) {
 
-    listaBonita[i].addEventListener('click', function() {
-      let newColor = 'rgb(128,128,128)';
-      if( listaBonita[i].style.backgroundColor === newColor) {
-          listaBonita[i].style.backgroundColor = backgroundColor;
-
-      }
-      else {listaBonita[i].style.backgroundColor === newColor
-      }
-      
-    })
-  }
-}
-
-//function changeBackground() {
- // let getListasTarefas = document.querySelector('#listas-tarefas');
-  //let getClassItem = 
-  //let backgroundColor = 'rgb(255, 128, 128)';
-  //let setColor = 'rgb(128, 128, 128';
