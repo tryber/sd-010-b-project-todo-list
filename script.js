c3247d09588a2782800dbeffbd5f2008e4273b45
lucas <peixotolucas516@gmail.com>
2021-03-28 20:29:11 -0300

    botao salvar

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const selected = document.querySelector('.selected')
-const completed = document.querySelector('.completed')
-function addNewTask() { //cria lista de tarefas apertando botao
+
+
+// function addNewTask() { //cria lista de tarefas apertando botao
   const getInputField = document.querySelector('#texto-tarefa'); 
   const addInputButton = document.querySelector('#criar-tarefa');
   const getTaskList = document.querySelector('#lista-tarefas');
@@ -9,16 +9,19 @@ function addNewTask() { //cria lista de tarefas apertando botao
   const eraseSelected = document.querySelector('#remover-selecionado');
   const selected = document.querySelector('.selected');
   const completed = document.querySelector('.completed');
-
+  const saveTasks = document.querySelector('#salvar-tarefas')
+  
   
 
   addInputButton.addEventListener('click', function() {
+    const newLi = document.createElement('li');
     if (getInputField.value.length > 0) {
-      const newLi = document.createElement('li');
+      
       newLi.innerText = getInputField.value;
 
       getTaskList.appendChild(newLi);
       getInputField.value = '';
+    }
       newLi.addEventListener('click', function(event) {
         // pintar a li de cinza e despintar as outras se houver
         let selected = document.querySelector('.selected')
@@ -27,33 +30,55 @@ function addNewTask() { //cria lista de tarefas apertando botao
         }
         event.target.classList.add('selected')
       })
+    
       newLi.addEventListener('dblclick', function(event){
         event.target.classList.toggle('completed')
       })
+
       eraseAllButton.addEventListener('click', function(event) {
-        getTaskList.removeChild(newLi)
+        
+          getTaskList.innerHTML = '';
+             
       })
+
       eraseFinished.addEventListener('click', function(event){
         let completed = document.querySelector('.completed');
           if (completed != null) {
             completed.remove();
           }
-         
       })
       eraseSelected.addEventListener('click', function(event){
         let selected = document.querySelector('.selected');
           if (selected != null) {
             selected.remove();
           }
-        
       })
-    }    
+      saveTasks.addEventListener('click', () => {
+        localStorage.setItem('key', getTaskList.innerHTML);
+        
+      });
       
-  })
+         
+      
+        
+    
+  });
   
-};
+// };
+const localList = (event) => {
+  if (localStorage.getItem('key') !== undefined) {
+    const saved = localStorage.getItem('key');
+    
+  getTaskList.innerHTML = saved;
+  }
 
-addNewTask();
+}
+
+window.onload = () => {
+  // addNewTask();
+  localList();
+
+}
 
 
 
