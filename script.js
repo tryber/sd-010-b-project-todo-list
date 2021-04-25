16bb59a78a26f184643f7444800e7c98ed74ace5
renatogsouza <renatomacedo1995@gmail.com>
2021-04-25 14:30:41 -0300

    selecionando um item da lista

--- script.js
+++ script.js
@@ -2,11 +2,27 @@ const taskList = document.getElementById('lista-tarefas');
 const btnCriarTarefa = document.getElementById('criar-tarefa');
 const taskText = document.getElementById('texto-tarefa');
 
+const checkBackgroundColor = () => {
+  const li = document.querySelector('.colorGray');
+  if(li){
+    li.className = '';
+  }
+}
+
+const selectTask = (event) => {
+  checkBackgroundColor();
+  event.target.classList.add('colorGray');
+}
+
 const createList = () =>{
   const creatLi = document.createElement('li');
+
   creatLi.innerText = taskText.value;
   taskText.value = '';
+
   taskList.appendChild(creatLi);
+
+  creatLi.addEventListener('click', selectTask);
   taskText.focus();
 };
 btnCriarTarefa.addEventListener('click', () => {
