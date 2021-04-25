37c7d2bdf592167003b43c5360201e39f35ca0c6
renatogsouza <renatomacedo1995@gmail.com>
2021-04-25 15:14:40 -0300

    corrigindo bug de ao clicar no item da lista ele desmarca

--- script.js
+++ script.js
@@ -5,23 +5,33 @@ const taskText = document.getElementById('texto-tarefa');
 const checkBackgroundColor = () => {
   const li = document.querySelector('.colorGray');
   if(li){
-    li.className = '';
+  li.classList.remove('colorGray');
   }
 }
 
+const taskCompleted = (event) => {
+  event.target.classList.toggle('completed');
+}
+
 const selectTask = (event) => {
+  if(event.target.className === 'colorGray'){
+  event.target.classList.remove('colorGray');
+  } else {
   checkBackgroundColor();
   event.target.classList.add('colorGray');
+  }
 }
 
 const createList = () =>{
   const creatLi = document.createElement('li');
 
+  creatLi.className= '';
   creatLi.innerText = taskText.value;
   taskText.value = '';
 
   taskList.appendChild(creatLi);
 
+  creatLi.addEventListener('dblclick', taskCompleted);
   creatLi.addEventListener('click', selectTask);
   taskText.focus();
 };
